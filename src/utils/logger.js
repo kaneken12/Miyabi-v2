174cc20005fdf8202f01589d9bc9073aab6c20e1
Chrysler-Lexus <kenkaneky244@gmail.com>
2026-04-23 22:28:25 +0100

    Create utils/logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,15 @@
+const pino = require('pino');
+
+const logger = pino({
+    level: 'info',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+            ignore: 'pid,hostname'
+        }
+    }
+});
+
+module.exports = logger;
